44b8ddf3bfc0100125b2ac8a9fcc8a3554d62914
Martin Stannard <martin@stannard.id.au>
2016-04-18 23:46:20 +1000

    Added Player type and view, split board into rows, added Clear function, crappy css.

--- apps/skateboard/web/elm/GameBoard.elm
+++ apps/skateboard/web/elm/GameBoard.elm
@@ -6,6 +6,25 @@ import Html.Events as Events
 import StartApp.Simple
 
 
+main : Signal.Signal Html
+main =
+  StartApp.Simple.start
+    { model = initialModel
+    , view = view
+    , update = update
+    }
+
+
+
+--MODELS
+
+
+type alias Player =
+  { name : String
+  , score : Int
+  }
+
+
 type alias Letter =
   { char :
       String
@@ -20,78 +39,129 @@ type alias Board =
 
 type alias Model =
   { candidate : String
-  , board : List Letter
+  , board :
+      List (List Letter)
+  , players : List Player
   }
 
 
 type Action
   = NoOp
-  | Submit
   | Select Letter
+  | Submit String
+  | Clear
+  | UpdateBoard Model
 
 
 initialModel : Model
 initialModel =
   { candidate = ""
   , board =
-      [ { char = "A" }
-      , { char = "B" }
-      , { char = "D" }
-      , { char = "F" }
-      , { char = "U" }
-      , { char = "O" }
-      , { char = "K" }
-      , { char = "N" }
-      , { char = "M" }
-      , { char = "P" }
-      , { char = "J" }
-      , { char = "R" }
-      , { char = "S" }
-      , { char = "E" }
-      , { char = "H" }
-      , { char = "H" }
-      , { char = "M" }
-      , { char = "L" }
-      , { char = "L" }
+      [ [ { char = "A" }
+        , { char = "G" }
+        , { char = "S" }
+        , { char = "Z" }
+        , { char = "L" }
+        ]
+      , [ { char = "L" }
+        , { char = "B" }
+        , { char = "P" }
+        , { char = "I" }
+        , { char = "U" }
+        ]
+      , [ { char = "T" }
+        , { char = "T" }
+        , { char = "D" }
+        , { char = "F" }
+        , { char = "O" }
+        ]
+      , [ { char = "C" }
+        , { char = "D" }
+        , { char = "D" }
+        , { char = "G" }
+        , { char = "U" }
+        ]
+      , [ { char = "X" }
+        , { char = "D" }
+        , { char = "U" }
+        , { char = "E" }
+        , { char = "R" }
+        ]
+      ]
+  , players =
+      [ { name = "martin", score = 10 }
+      , { name = "bob", score = 13 }
+      , { name = "jane", score = 30 }
       ]
   }
+
+
+
+--UPDATE
 
 
 update : Action -> Model -> Model
 update action model =
   case action of
-    Submit ->
+    Submit word ->
       model
 
     Select letter ->
       { model | candidate = model.candidate ++ letter.char }
 
+    Clear ->
+      { model | candidate = "" }
+
     _ ->
       model
+
+
+
+--VIEWS
 
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div
-    []
-    [ h2 [] [ text model.candidate ]
+    [ class "row" ]
+    [ div
+        [ class "jumbotron" ]
+        [ h3 [] [ text "LettersMash" ] ]
     , div
-        []
-        (List.map (letterView address) model.board)
+        [ class "board col-md-8" ]
+        (List.map (boardRow address) model.board)
+    , div
+        [ class "col-md-4" ]
+        (List.map playerView model.players)
+    , div
+        [ class "col-md-12" ]
+        [ button [ Events.onClick address Clear ] [ text "Clear" ]
+        , button [ Events.onClick address (Submit model.candidate) ] [ text "Submit" ]
+        , h2 [] [ text model.candidate ]
+        ]
     ]
+
+
+playerView : Player -> Html
+playerView player =
+  div
+    []
+    [ h3 [] [ text player.name ] ]
+
+
+boardRow : Signal.Address Action -> List Letter -> Html
+boardRow address letters =
+  div
+    []
+    (List.map (letterView address) letters)
 
 
 letterView : Signal.Address Action -> Letter -> Html
 letterView address model =
   span
-    [ Events.onClick address (Select model), style [ ( "font-size", "150%" ) ] ]
+    [ class "letter", Events.onClick address (Select model), style [ ( "font-size", "150%" ) ] ]
     [ text model.char ]
 
 
-main : Signal.Signal Html
-main =
-  StartApp.Simple.start
-    { model = initialModel
-    , view = view
-    , update = update
-    }
+
+--SIGNALS
