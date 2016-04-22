9c91ba2ec922fc2b606a690829f098120f81f238
Owen Horner <owenhorner@live.com.au>
2016-04-22 12:41:11 +1000

    adds specs for dictionary

---
+++ apps/library/test/library/dictionary_test.exs
@@ -0,0 +1,24 @@
+defmodule Library.DictionaryTest do
+  use ExUnit.Case
+  alias Library.Dictionary
+
+  test "word is valid" do
+    word = "hello"
+    assert Dictionary.check_word(word) == {:valid, word}
+  end
+
+  test "proper nouns are downcased" do
+    word = "Owen"
+    assert Dictionary.check_word(word) == {:valid, String.downcase(word)}
+  end
+
+  test "word is invalid" do
+    word = "sdoihfop"
+    assert Dictionary.check_word(word) == {:invalid, word}
+  end
+
+  test "word which contains numbers is invalid" do
+    word = "12345"
+    assert Dictionary.check_word(word) == {:invalid, word}
+  end
+end
