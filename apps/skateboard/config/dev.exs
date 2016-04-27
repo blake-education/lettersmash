21ce30a7ee9bfc494963459aec2b3a2a8e27af57
Shamim <shamim.jahandideh@gmail.com>
2016-04-27 22:33:23 +1000

    wip

--- apps/skateboard/config/dev.exs
+++ apps/skateboard/config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :skateboard, Skateboard.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "shamim",
+  password: "",
   database: "skateboard_dev",
   hostname: "localhost",
   pool_size: 10
---
+++ apps/skateboard/web/channels/game_channel.ex
@@ -0,0 +1,9 @@
+defmodule Skateboard.GameChannel do
+  use Phoenix.Channel
+
+  def join("game:new", _message, socket) do
+    id = socket.assigns.user_id
+    user = Skateboard.Repo.get(User, id)
+    {:ok, %{user_id: user.id, user_name: user.name}, socket}
+  end
+end
