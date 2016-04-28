e1e9a1983c9bd4bed8e5eb472aa2eedadc01519a
Shamim Jahandideh <shamim.jahandideh@blake.com.au>
2016-04-28 16:47:46 +1000

    changing dev back to normal

--- apps/skateboard/config/dev.exs
+++ apps/skateboard/config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :skateboard, Skateboard.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "shamim",
-  password: "",
+  username: "postgres",
+  password: "postgres",
   database: "skateboard_dev",
   hostname: "localhost",
   pool_size: 10
