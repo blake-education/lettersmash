b1a6fdd1564b9febc594e774ad9ee924f73c0976
Martin Stannard <martin@stannard.id.au>
2016-05-02 08:25:06 +1000

    Added a Board module to encapsulate most board logic

---
+++ apps/library/test/library/board_test.exs
@@ -0,0 +1,27 @@
+defmodule Library.BoardTest do
+  use ExUnit.Case
+  alias Library.Board
+
+  test "initial board is not completed" do
+    board = Board.generate
+    assert Board.completed?(board) == false
+  end
+
+  test "completely owned board is completed" do
+    board = [%{owner: 1}, %{owner: 2}]
+    assert Board.completed?(board) == true
+  end
+
+  test "generates 25 letters initially" do
+    board = Board.generate
+    assert Enum.count(board) == 25
+  end
+
+  test "adding a word replaces the owner of each letter" do
+    word = [%{"id" => 2, "owner" => "1"}, %{"id" => 4, "owner" => "1"}]
+    updated_board = Board.add_word(word, "1", Board.generate)
+    owner_1_count = Enum.count(updated_board, &(&1["owner"] == 1))
+    assert owner_1_count == 2
+  end
+
+end
