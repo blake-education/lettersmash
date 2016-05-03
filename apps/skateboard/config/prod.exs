8bec2a53818932e5cd5eafb1056c5108fdbd595f
Martin Stannard <martin@stannard.id.au>
2016-05-03 15:35:54 +1000

    add missin comma

--- apps/skateboard/config/prod.exs
+++ apps/skateboard/config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :skateboard, Skateboard.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
