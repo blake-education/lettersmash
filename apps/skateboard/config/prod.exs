376c5a1d28ca4bad0c12ec31e7ec3dca78e5a74d
Martin Stannard <martin@stannard.id.au>
2016-05-03 15:34:14 +1000

    add prod config with ENV vars

--- apps/skateboard/config/prod.exs
+++ apps/skateboard/config/prod.exs
@@ -15,6 +15,15 @@ config :skateboard, Skateboard.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+#config :skateboard, Skateboard.Repo,
+  #adapter: Ecto.Adapters.Postgres,
+  #username: System.get_env("DATABASE_USERNAME"),
+  #password: System.get_env("DATABASE_PASSWORD"),
+  #database: "hello_phoenix_heroku_prod",
+  #size: 20 # The amount of database connections in the pool
 
 # Do not print debug messages in production
 config :logger, level: :info
