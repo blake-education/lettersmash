5e04aa8521ad4e18828ced5fe290e0c827419710
Martin Stannard <martin@stannard.id.au>
2016-05-06 00:34:42 +1000

    Move models into separate file

---
+++ apps/skateboard/web/elm/Models.elm
@@ -0,0 +1,58 @@
+module Models (..) where
+
+
+type alias Status =
+  String
+
+
+type alias Letter =
+  { letter : String
+  , id : Int
+  , owner : Int
+  , surrounded : Bool
+  }
+
+
+type alias Player =
+  { name : String
+  , score : Int
+  }
+
+
+type alias Candidate =
+  List Letter
+
+
+type alias BoardState =
+  { board : Board
+  , players : List Player
+  , wordlist : List String
+  }
+
+
+type alias BoardRow =
+  List Letter
+
+
+type alias Board =
+  List BoardRow
+
+
+type alias Model =
+  { candidate : Candidate
+  , boardState : BoardState
+  , errorMessage : String
+  }
+
+
+initialModel : Model
+initialModel =
+  { candidate = []
+  , boardState =
+      { board = []
+      , players = []
+      , wordlist = []
+      }
+  , errorMessage = "Welcome to LettersMash"
+  }
+
