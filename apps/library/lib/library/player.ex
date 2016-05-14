0304e1e548f82af3db0a292f99702596bc06ea37
Martin Stannard <martin@stannard.id.au>
2016-05-14 10:35:00 +1000

    Added functions for total_score, games_won and games_played

--- apps/library/lib/library/player.ex
+++ apps/library/lib/library/player.ex
@@ -1,6 +1,7 @@
 defmodule Library.Player do
   alias Skateboard.Event
   alias Skateboard.Repo
+  import Ecto.Query, only: [from: 2]
 
   defstruct name: "", score: 0, id: nil, index: nil
 
@@ -11,6 +12,27 @@ defmodule Library.Player do
       score: player.score,
       winner: false
     })
+  end
+
+  def total_score(player_id) do
+    query = from e in Event,
+      where: e.user_id == ^player_id,
+      select: sum(e.score)
+    Repo.one query
+  end
+
+  def games_played(player_id) do
+    query = from e in Event,
+      where: e.user_id == ^player_id,
+      select: count(e.game_id, :distinct)
+    Repo.one query
+  end
+
+  def games_won(player_id) do
+    query = from e in Event,
+      where: e.user_id == ^player_id and e.winner == true,
+      select: count(e.winner)
+    Repo.one query
   end
 
 end
