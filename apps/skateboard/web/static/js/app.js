b113640f0c1e19f7b55ea7eb45a71cf45d090f63
Garrett Heinlen <heinleng@gmail.com>
2016-05-16 09:27:38 +1000

    all the js compiled ever.

--- apps/skateboard/web/static/js/app.js
+++ apps/skateboard/web/static/js/app.js
@@ -1,65 +1,72 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
 import socket from "./socket"
 
 const elmDiv = document.getElementById('elm-container');
-const elmApp = Elm.GameBoard.embed(elmDiv);
 
-/* we will do this via channels eventually*/
-//elmApp.ports.boardState.send(initialState.boardState);
+if (elmDiv) {
+  const elmApp = Elm.GameBoard.embed(elmDiv);
 
-// CHANNELS
-let channel = socket.channel("game:new", {})
-channel.join()
-  .receive("ok", resp => {
-    console.log("Joined successfully", resp);
-    channel.push("board_state")
+  // CHANNELS
+  let channel = socket.channel("game:new", {})
+  channel.join()
+    .receive("ok", resp => {
+      console.log("Joined successfully", resp);
+      channel.push("board_state")
+    })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  channel.on("board_state", board_state => {
+    console.log("board state: ", board_state);
+    elmApp.ports.boardState.send(board_state);
   })
-  .receive("error", resp => { console.log("Unable to join", resp) })
 
-channel.on("board_state", board_state => {
-  console.log("board state: ", board_state);
-  elmApp.ports.boardState.send(board_state);
-})
+  channel.on("game_over", data => {
+    console.log("game_over: ", data);
+    elmApp.ports.gameOver.send(data.message);
+  })
 
-channel.on("game_over", data => {
-  console.log("game_over: ", data);
-  elmApp.ports.gameOver.send(data.message);
-})
+  channel.on("submission_successful", data => {
+    console.log("submission_successful: ", data);
+    elmApp.ports.submitSuccess.send(data.message);
+  })
 
-channel.on("submission_successful", data => {
-  console.log("submission_successful: ", data);
-  elmApp.ports.submitSuccess.send(data.message);
-})
+  channel.on("submission_failed", data => {
+    console.log("submission_failed: ", data);
+    elmApp.ports.submitFailed.send(data.message);
+  })
 
-channel.on("submission_failed", data => {
-  console.log("submission_failed: ", data);
-  elmApp.ports.submitFailed.send(data.message);
-})
+  elmApp.ports.submit.subscribe( function(letters) {
+    channel.push("submit_word", letters);
+  });
 
-elmApp.ports.submit.subscribe( function(letters) {
-  channel.push("submit_word", letters);
-});
+  elmApp.ports.requestNewGame.subscribe( function(message) {
+    channel.push("new_game", {message: message});
+  });
 
-elmApp.ports.requestNewGame.subscribe( function(message) {
-  channel.push("new_game", {message: message});
-});
+}
 
+// Lobby app
+const elmDivLobby = document.getElementById('elm-lobby');
+if (elmDivLobby) {
+  const elmLobbyApp = Elm.GameLobby.embed(elmDivLobby);
+
+  let channel = socket.channel("lobby", {})
+  channel.join()
+    .receive("ok", resp => {
+      console.log("Joined successfully", resp);
+    })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+  channel.on("game_list", gameList => {
+    console.log("gameList: ", gameList);
+    elmLobbyApp.ports.gameList.send(gameList.games);
+  })
+
+  function createGame() {
+    console.log("CreateGame!")
+    channel.push("create_game")
+  };
+
+  const button = document.getElementById("create_game")
+  button.onclick = createGame;
+}
