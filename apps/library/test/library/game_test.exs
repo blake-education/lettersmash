4d47259b9cef50a8268e958289eb15fc025dce09
Martin Stannard <martin@stannard.id.au>
2016-05-21 12:07:44 +1000

    fix some tests

--- apps/library/test/library/game_test.exs
+++ apps/library/test/library/game_test.exs
@@ -1,38 +1,44 @@
 defmodule Library.GameTest do
   use ExUnit.Case
 
-  test "list_state - consist of board and player keys" do
-    {:ok, pid} = Library.Game.start_link
+  alias Library.Game
 
-    assert %{board: _, players: _} = Library.Game.list_state(pid)
+  setup do
+    {:ok, game} = Game.start_link("game1")
+    {:ok, game: game}
   end
 
-  test "add_player - adds a player to the state map" do
-    {:ok, pid} = Library.Game.start_link
+  test "can be started", %{game: game} do
+    assert game
+  end
 
-    Library.Game.add_player(pid, %{id: 1, name: "abc"})
-    assert %{board: _, players: players} = Library.Game.list_state(pid)
+  test "list_state - consist of board and player keys", %{game: game} do
+
+    assert %{board: _, players: _, wordlist: _, game_over: _} = Library.Game.list_state(game)
+  end
+
+  test "add_player - adds a player to the state map", %{game: game} do
+
+    Library.Game.add_player(game, %{id: 1, name: "abc"})
+    assert %{board: _, players: players} = Library.Game.list_state(game)
     assert length(players) == 1
   end
 
-  test "remove_player - adds a player to the state map" do
-    {:ok, pid} = Library.Game.start_link
-
-    Library.Game.add_player(pid, %{id: 1, name: "abc"})
-    assert %{board: _, players: players} = Library.Game.list_state(pid)
+  test "remove_player - removes a player to the state map", %{game: game} do
+    Library.Game.add_player(game, %{id: 1, name: "abc"})
+    assert %{board: _, players: players} = Library.Game.list_state(game)
     assert length(players) == 1
 
-    Library.Game.remove_player(pid, %{id: 1, name: "abc"})
-    assert %{board: _, players: players} = Library.Game.list_state(pid)
+    Library.Game.remove_player(game, %{id: 1, name: "abc"})
+    assert %{board: _, players: players} = Library.Game.list_state(game)
     assert length(players) == 0
   end
 
-  test "submit_word - with a valid word" do
-    {:ok, pid} = Library.Game.start_link
+  test "submit_word - with a valid word", %{game: game} do
 
-    word = "abc"
-    player = %{id: 1, name: "abc"}
+    word = [%{letter: "C", index: 1}, %{letter: "A", index: 2}, %{letter: "N", index: 3},%{letter: "E", index: 4}]
+    player = %{id: 1, name: "abc", index: 2}
     
-    assert {:ok, word} = Library.Game.submit_word(pid, word, player)
+    assert {:ok, word} = Library.Game.submit_word(game, word, player)
   end
 end
