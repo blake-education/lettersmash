97a305ca984a3ca1713df6661acea89a2f823e17
Martin Stannard <martin@stannard.id.au>
2016-06-06 19:07:36 +1000

    WIP on getting login working correctly

--- apps/skateboard/web/controllers/page_controller.ex
+++ apps/skateboard/web/controllers/page_controller.ex
@@ -3,11 +3,11 @@ defmodule Skateboard.PageController do
 
   def index(conn, _params) do
     IO.inspect conn.assigns
-    if conn.assigns.current_user do
-      redirect(conn, to: page_path(conn, :game))
-    else
+    # if conn.assigns.current_user do
+    #   redirect(conn, to: page_path(conn, :game))
+    # else
       render conn, "index.html"
-    end
+    # end
   end
 
   def game(conn, _params) do
